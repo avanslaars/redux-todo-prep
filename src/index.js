1ee1e9ce8a5ba6a4c4ecd249bf103e459ec5f9df
Andrew L. Van Slaars <avanslaars@gmail.com>
2017-05-24 23:25:41 -0400

    bindActionCreators

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import store from './store'
+import { bindActionCreators } from 'redux'
 import {updateCurrent} from './reducers/todo'
 
-const handleUpdateCurrent = (val) => store.dispatch(updateCurrent(val))
+const actions = bindActionCreators({updateCurrent}, store.dispatch)
 
 const render = () => {
   const state = store.getState()
-  ReactDOM.render(<App {...state} updateCurrent={handleUpdateCurrent} />, document.getElementById('root'));
+  ReactDOM.render(<App {...state} updateCurrent={actions.updateCurrent} />, document.getElementById('root'));
 }
 render()
 
