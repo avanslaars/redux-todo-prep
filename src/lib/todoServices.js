06649f2643883fb043333868307abad868682c42
Andrew L. Van Slaars <avanslaars@gmail.com>
2017-05-26 18:50:11 -0400

    Prepped for now deployment

--- src/lib/todoServices.js
+++ src/lib/todoServices.js
@@ -1,5 +1,7 @@
+const baseUrl = process.env.REACT_APP_BASE_URL
+
 export const createTodo = (name) => {
-  return fetch('http://localhost:8080/todos', {
+  return fetch(baseUrl, {
     method: 'POST',
     headers: {
     'Accept': 'application/json',
@@ -11,7 +13,7 @@ export const createTodo = (name) => {
 }
 
 export const updateTodo = (todo) => {
-  return fetch(`http://localhost:8080/todos/${todo.id}`, {
+  return fetch(`${baseUrl}/${todo.id}`, {
     method: 'PUT',
     headers: {
     'Accept': 'application/json',
@@ -23,7 +25,7 @@ export const updateTodo = (todo) => {
 }
 
 export const destroyTodo = (id) => {
-  return fetch(`http://localhost:8080/todos/${id}`, {
+  return fetch(`${baseUrl}/${id}`, {
     method: 'DELETE',
     headers: {
       'Accept': 'application/json',
@@ -33,6 +35,6 @@ export const destroyTodo = (id) => {
 }
 
 export const getTodos = () => {
-  return fetch('http://localhost:8080/todos')
+  return fetch(baseUrl)
     .then(res => res.json())
 }
